f4fa54ce3e6be1e0b5230ac8968a40c9779c360b
Jonathan Jiang <zhqjiang@outlook.com>
2021-10-24 22:46:03 +0800

    feat: add @better-scroll

--- src/components/picker.tsx
+++ src/components/picker.tsx
@@ -1,7 +1,18 @@
 import React, { FC } from "react";
 import "./picker.scss";
+import BScroll from '@better-scroll/core'
+import Wheel from '@better-scroll/wheel'
 
 interface IProps {}
+BScroll.use(Wheel)
+
+const STATE_HIDE = 0
+const STATE_SHOW = 1
+
+const COMPONENT_NAME = 'picker'
+const EVENT_SELECT = 'select'
+const EVENT_CANCEL = 'cancel'
+const EVENT_CHANGE = 'change'
 
 const Picker: FC<IProps> = () => {
   return <div>Picker</div>;
